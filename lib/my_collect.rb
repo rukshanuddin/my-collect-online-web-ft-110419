1be60d0a748472537c139a4893cc44a88779977d
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-04 22:44:39 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,9 @@
 def my_collect(array) # put argument(s) here
   i = 0
-  ary = []
+  
     while i < array.length
-      yield ary << array[i]
+      yield array[i]
       i = i + 1
     end
-    
+
 end
